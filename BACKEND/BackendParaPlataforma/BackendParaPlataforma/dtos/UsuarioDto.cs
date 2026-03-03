598beea7dcde170452dc925038dfe9fa2f8f3058
natalia-123 <nlarosa@ucm.es>
2026-03-03 17:51:16 +0100

    usuariodto

---
+++ BACKEND/BackendParaPlataforma/BackendParaPlataforma/dtos/UsuarioDto.cs
@@ -0,0 +1,21 @@
+﻿namespace BackendParaPlataforma.dtos
+{
+    public class UsuarioDto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public DateTime FechaRegistro { get; set; }
+
+        public DateTime? UltimoLogin { get; set; }
+
+        public string? EstadoActual { get; set; }
+
+        public string? ConfiguracionVoz { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
