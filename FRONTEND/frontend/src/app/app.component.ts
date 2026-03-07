8403ec7053a91ef151710cc6e026349afb6cd3c5
natalia-123 <nlarosa@ucm.es>
2026-03-07 22:31:44 +0100

    #16 Maquetación de register y login  LOGIN DE PRUEBA

--- FRONTEND/frontend/src/app/app.component.ts
+++ FRONTEND/frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,15 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login("email", "password")
+      .subscribe(res => {
+        console.log(res);
+        localStorage.setItem('token', res.token);
+      });
+  }
   title = 'frontend';
 }
