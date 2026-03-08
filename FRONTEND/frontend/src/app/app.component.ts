55f70a5c64f6a97fd95208a9da64e1200aa43255
iremariv-sys <iremariv@ucm.es>
2026-03-08 15:47:19 +0100

    #3 home, diary

--- FRONTEND/frontend/src/app/app.component.ts
+++ FRONTEND/frontend/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [NavComponent, HomeComponent, HeaderComponent, RouterOutlet],
+  //templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  template: `
+   <app-nav></app-nav>
+    <app-header></app-header>
+   
+    <router-outlet></router-outlet>
+  
+  `
 })
 export class AppComponent {
   title = 'frontend';
--- FRONTEND/frontend/src/app/app.routes.ts
+++ FRONTEND/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DiaryComponent } from './diary/diary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'diary', component: DiaryComponent }
+];
