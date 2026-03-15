2be46855f129ad24bbda82c5058212ad8cae9a35
iremariv-sys <iremariv@ucm.es>
2026-03-15 21:08:45 +0100

    #10 Home litp

--- FRONTEND/frontend/src/app/app.component.ts
+++ FRONTEND/frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './components/header/header.component';
+//import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [NavComponent, HomeComponent, HeaderComponent, RouterOutlet, RouterLink],
+  imports: [NavComponent, HomeComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
