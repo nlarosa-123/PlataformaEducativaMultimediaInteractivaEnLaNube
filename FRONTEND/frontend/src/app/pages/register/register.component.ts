e70f09d5edcf7cb7e98dc7ba3b04ce326dc37911
iremariv-sys <iremariv@ucm.es>
2026-03-16 18:06:10 +0100

    #17 maquetado de login

--- FRONTEND/frontend/src/app/pages/register/register.component.ts
+++ FRONTEND/frontend/src/app/pages/register/register.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
