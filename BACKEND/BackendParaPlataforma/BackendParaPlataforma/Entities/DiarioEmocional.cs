a05c9d0b08061cfd1c40971774dccd4719785013
AlonsoCampi09 <alocam01@ucm.es>
2026-04-04 13:18:30 +0200

    cambios en el .gitignore para que no suba la configuracion de sql

---
+++ BACKEND/BackendParaPlataforma/BackendParaPlataforma/Entities/DiarioEmocional.cs
@@ -0,0 +1,25 @@
+namespace BackendParaPlataforma.Entities
+{
+    public class DiarioEmocional
+    {
+        public int Id_Diario { get; set; }
+
+        public int Id_Usuario { get; set; }
+        public Usuario? Usuario { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public int Id_Emocion_Usuario { get; set; }
+        public Emociones? Emocion { get; set; }
+
+        public string? Texto_Usuario { get; set; }
+
+        public string? Audio_Url { get; set; }
+
+        public DateTime Fecha_Creacion { get; set; }
+
+        public AnalisisIA? AnalisisIA { get; set; }
+
+        public ReflexionMejora? ReflexionMejora { get; set; }
+    }
+}
