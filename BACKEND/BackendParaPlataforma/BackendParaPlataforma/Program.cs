3f1d728219112257bcc76bb049860986790612c8
AlonsoCampi09 <alocam01@ucm.es>
2026-04-04 13:37:18 +0200

    recuperado el appsetting y el Program.cs

---
+++ BACKEND/BackendParaPlataforma/BackendParaPlataforma/Program.cs
@@ -0,0 +1,81 @@
+using BackendParaPlataforma.Infraestructure.Mappings;
+using BackendParaPlataforma.Infraestructure.Repositories;
+using BackendParaPlataforma.Infraestructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using BackendParaPlataforma.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<JwtService>();
+
+var key = builder.Configuration["Jwt:Key"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(key!)
+        )
+    };
+});
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+   options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IEmocionesRepository, EmocionesRepository>();
+builder.Services.AddScoped<IProgresoModuloUsuarioRepository, ProgresoModuloUsuarioRepository>();
+builder.Services.AddScoped<IDiarioEmocionalRepository, DiarioEmocionalRepository>();
+builder.Services.AddScoped<IAnalisisIARepository, AnalisisIARepository>();
+builder.Services.AddScoped<IReflexionMejoraRepository, ReflexionMejoraRepository>();
+builder.Services.AddScoped<IProgresoRepository, ProgresoRepository>();
+builder.Services.AddScoped<IEstadisticaUsuarioRepository, EstadisticaUsuarioRepository>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirAngular",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+
+app.UseCors("PermitirAngular");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
