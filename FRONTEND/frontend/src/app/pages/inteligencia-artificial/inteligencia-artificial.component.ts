793acfbe7fe356f14a3dc7346c4baa16b0c9e650
natalia-123 <nlarosa@ucm.es>
2026-04-04 02:40:45 +0200

    #34 inteligencia artificial component

---
+++ FRONTEND/frontend/src/app/pages/inteligencia-artificial/inteligencia-artificial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inteligencia-artificial',
+  imports: [],
+  templateUrl: './inteligencia-artificial.component.html',
+  styleUrl: './inteligencia-artificial.component.scss'
+})
+export class InteligenciaArtificialComponent {
+
+}
