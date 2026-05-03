688a9697009f03ecadd5b25a7bc620f55d8c6de7
natalia-123 <nlarosa@ucm.es>
2026-05-03 17:00:06 +0200

    #46 Azure Sentiment Analysis Frontend

--- FRONTEND/frontend/src/app/pages/diario-emocional-ia/diario-emocional-ia.component.ts
+++ FRONTEND/frontend/src/app/pages/diario-emocional-ia/diario-emocional-ia.component.ts
@@ -18,6 +18,14 @@ export class DiarioEmocionalIAComponent implements OnInit {
   tonoAzure: string = '';
   tonoAzureTexto: string = '';
 
+  openAIAnalisis: any;
+  tonoOpenAI: string = '';
+  tonoOpenAITexto: string = '';
+
+  AWSAnalisis: any;
+  tonoAWS: string = '';
+  tonoAWSTexto: string = '';
+
   constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
@@ -39,9 +47,21 @@ export class DiarioEmocionalIAComponent implements OnInit {
         this.analisisList = res;
 
         // 🔥 separar Azure del resto
-        this.azureAnalisis = res.find(x => x.provider === 'Azure');
+        this.azureAnalisis = res.find(x => x.provider?.toLowerCase() === 'azure');
         this.tonoAzure = this.azureAnalisis?.sentiment?.toLowerCase();
         this.tonoAzureTexto = this.getTextoTonoAzure();
+
+        // region OpenAI
+        this.openAIAnalisis = res.find(x => x.provider?.toLowerCase() === 'openai');
+        this.tonoOpenAI = this.openAIAnalisis?.sentiment?.toLowerCase();
+        this.tonoOpenAITexto = this.getTextoTonoOpenAI();
+        // endregion OpenAI
+
+        // region AWS
+        this.AWSAnalisis = res.find(x => x.provider?.toLowerCase() === 'aws');
+        this.tonoAWS = this.AWSAnalisis?.sentiment?.toLowerCase();
+        this.tonoAWSTexto = this.getTextoTonoAWS();
+        // endregion AWS
 
         console.log(this.analisisList);
       },
@@ -71,6 +91,55 @@ export class DiarioEmocionalIAComponent implements OnInit {
 }
   });
 }
+//endregion Azure
+
+//region OpenAI
+  responderOpenAI(valor: boolean) {
+  if (!this.openAIAnalisis) return;
+
+  const payload = {
+    ...this.openAIAnalisis,
+    coincide_Usuario: valor,
+    fecha_Analisis: new Date().toISOString()
+  };
+
+  this.http.put(
+    `http://localhost:5169/api/SentimentResult/${this.openAIAnalisis.id_Analisis}`,
+    payload
+  ).subscribe({
+    next: () => {
+      this.openAIAnalisis.coincide_Usuario = valor;
+    },
+    error: (err) => {
+  console.error(err);
+}
+  });
+}
+//endregion OpenAI
+
+//region AWS
+  responderAWS(valor: boolean) {
+  if (!this.AWSAnalisis) return;
+
+  const payload = {
+    ...this.AWSAnalisis,
+    coincide_Usuario: valor,
+    fecha_Analisis: new Date().toISOString()
+  };
+
+  this.http.put(
+    `http://localhost:5169/api/SentimentResult/${this.AWSAnalisis.id_Analisis}`,
+    payload
+  ).subscribe({
+    next: () => {
+      this.AWSAnalisis.coincide_Usuario = valor;
+    },
+    error: (err) => {
+  console.error(err);
+}
+  });
+}
+//endregion OpenAI
 
   getTextoTonoAzure(): string {
   switch (this.tonoAzure) {
@@ -84,6 +153,36 @@ export class DiarioEmocionalIAComponent implements OnInit {
       return 'mixto 🤔';
     default:
       return this.tonoAzure;
+  }
+}
+
+  getTextoTonoOpenAI(): string {
+  switch (this.tonoOpenAI) {
+    case 'positive':
+      return 'positivo 😊';
+    case 'neutral':
+      return 'neutral 😐';
+    case 'negative':
+      return 'negativo 😞';
+    case 'mixed':
+      return 'mixto 🤔';
+    default:
+      return this.tonoOpenAI;
+  }
+}
+
+  getTextoTonoAWS(): string {
+  switch (this.tonoAWS) {
+    case 'positive':
+      return 'positivo 😊';
+    case 'neutral':
+      return 'neutral 😐';
+    case 'negative':
+      return 'negativo 😞';
+    case 'mixed':
+      return 'mixto 🤔';
+    default:
+      return this.tonoAWS;
   }
 }
 
